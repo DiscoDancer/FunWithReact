c736e9851cad2980d515fbdb7ff6af20ed1149f1
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-06-13 16:22:55 +0300

    first version

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export interface InterfaceAppProps {
+    testMessage: string
+}
+
+class App extends React.Component<InterfaceAppProps, undefined> {
+    render() {
+        return (
+            <div>
+                {this.props.testMessage}
+            </div>
+        );
+    }
+}
+
+export default App;
